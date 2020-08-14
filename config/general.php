e7efec22a2e3a4f2cabef12ec10f085a93f66b8f
Kyle Andrews <codingwithkyle@gmail.com>
2020-08-13 20:21:22 -0400

    Installed Craft 3.5 and added initial page builder templates

---
+++ config/general.php
@@ -0,0 +1,55 @@
+<?php
+/**
+ * General Configuration
+ *
+ * All of your system's general configuration settings go in here. You can see a
+ * list of the available settings in vendor/craftcms/cms/src/config/GeneralConfig.php.
+ *
+ * @see \craft\config\GeneralConfig
+ */
+
+use craft\helpers\App;
+
+return [
+    // Global settings
+    '*' => [
+        'defaultWeekStartDay'               => 1,
+        'omitScriptNameInUrls'              => true,
+        'cpTrigger'                         => 'webmaster',
+        'securityKey'                       => getenv('SECURITY_KEY'),
+        'useProjectConfigFile'              => true,
+        'useEmailAsUsername'                => true,
+        'setPasswordPath'                   => 'users/set-password',
+        'autoLoginAfterAccountActivation'   => true,
+    ],
+
+    // Dev environment settings
+    'dev' => [
+        'siteUrl'               => getenv('DEV_URL'),
+        'devMode'               => true,
+        'allowUpdates'          => true,
+        'enableTemplateCaching' => false,
+        'testToEmailAddress'    => getenv('DEV_EMAIL_ADDRESS'),
+        'aliases' => [
+            '@rootUrl' => getenv('DEV_URL'),
+        ],
+    ],
+
+    // Staging environment settings
+    'staging' => [
+        'siteUrl'           => getenv('STAGING_URL'),
+        'allowAdminChanges' => false,
+        'aliases' => [
+            '@rootUrl' => getenv('STAGING_URL'),
+        ],
+    ],
+
+    // Production environment settings
+    'production' => [
+        'siteUrl'           => getenv('PRODUCTION_URL'),
+        'allowAdminChanges' => false,
+        'aliases' => [
+            '@rootUrl' => getenv('PRODUCTION_URL'),
+        ],
+    ],
+];
