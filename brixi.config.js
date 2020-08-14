fe76752844d60f60ce2ff804aaf07ff37390df2c
Kyle Andrews <codingwithkyle@gmail.com>
2020-08-13 21:12:50 -0400

    Styled example blocks

---
+++ brixi.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+    outDir: "./brixi",
+    important: true,
+    output: "production",
+    fonts: {
+        sizes: {
+            "4xl": 3,
+            "5xl": 4,
+            "6xl": 5,
+        },
+    },
+}
