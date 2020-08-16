bab5edea7ab8f630c1a795e6ba4e70afcc4d8fbc
Kyle Andrews <codingwithkyle@gmail.com>
2020-08-15 20:34:26 -0400

    Added ability to share page layouts & updated header button styles

--- preact/page-builder/index.tsx
+++ preact/page-builder/index.tsx
@@ -30,6 +30,7 @@ type PageBuilderState = {
     title: string;
     groups: Array<BlockGroup>;
     blocks: Array<IBlock>;
+    ready: boolean;
     blockData: {
         [key: string]: BlockData;
     };
@@ -55,6 +56,7 @@ class PageBuilder extends Component<{}, PageBuilderState> {
         this.view = createRef();
 
         this.state = {
+            ready: false,
             title: "Untitled page",
             groups: [],
             blocks: [],
@@ -309,6 +311,42 @@ class PageBuilder extends Component<{}, PageBuilderState> {
         }, 150);
     }
 
+    private setInitialState() {
+        let blocks = new URLSearchParams(location.search).getAll("b");
+        let loaded = 0;
+        new Promise((resolve) => {
+            if (blocks.length === 0) {
+                resolve();
+            }
+            for (let i = 0; i < blocks.length; i++) {
+                this.loadBlock(blocks[i]).then(() => {
+                    loaded++;
+                    if (blocks.length === loaded) {
+                        resolve();
+                    }
+                });
+            }
+        }).then(() => {
+            window.history.replaceState(null, null, `${location.origin}/${mountingPoint.dataset.cpTrigger}/papertrain/page-builder`);
+            this.setState({ view: blocks, ready: true });
+            setTimeout(() => {
+                this.view.current.scrollTo({
+                    top: 0,
+                    left: 0,
+                    behavior: "auto",
+                });
+                this.setState({
+                    drag: {
+                        over: false,
+                        handle: null,
+                        index: null,
+                        scrollDirection: 0,
+                    },
+                });
+            }, 150);
+        });
+    }
+
     // Event Listeners =========================================================================================================
 
     private dragOver: EventListener = (e: DragEvent) => {
@@ -333,7 +371,9 @@ class PageBuilder extends Component<{}, PageBuilderState> {
     };
 
     private handleReset: EventListener = () => {
-        this.setState({ view: [] });
+        if (window.confirm("Are you sure you want to trash this page layout?")) {
+            this.setState({ view: [] });
+        }
     };
 
     private startBodyDrag: EventListener = (e: MouseEvent) => {
@@ -366,6 +406,9 @@ class PageBuilder extends Component<{}, PageBuilderState> {
     };
 
     private createPage: EventListener = async () => {
+        if (!this.state.view.length) {
+            return;
+        }
         this.setState({ submitting: true });
         const data = new FormData();
         data.append("CRAFT_CSRF_TOKEN", mountingPoint.dataset.csrf);
@@ -406,6 +449,21 @@ class PageBuilder extends Component<{}, PageBuilderState> {
         } else {
             this.setState({ submitting: true });
         }
+    };
+
+    private handleURLPrompt: EventListener = () => {
+        let url = `${location.origin}/${mountingPoint.dataset.cpTrigger}/papertrain/page-builder`;
+        for (let i = 0; i < this.state.view.length; i++) {
+            switch (i) {
+                case 0:
+                    url += `?b=${this.state.view[i]}`;
+                    break;
+                default:
+                    url += `&b=${this.state.view[i]}`;
+                    break;
+            }
+        }
+        prompt("Share this page layout.", url);
     };
 
     // Render Functions =========================================================================================================
@@ -492,6 +550,8 @@ class PageBuilder extends Component<{}, PageBuilderState> {
         document.addEventListener("keyup", this.handleKeyboardMove);
 
         this.scrollCallback();
+
+        this.setInitialState();
     }
 
     render() {
@@ -511,8 +571,17 @@ class PageBuilder extends Component<{}, PageBuilderState> {
 
         let view: any = null;
         let dropzone = null;
-        if (this.state.view.length) {
+        if (this.state.view.length && this.state.ready) {
             view = this.state.view.map((handle, index) => this.renderBlock(handle, index));
+        } else if (!this.state.ready) {
+            view = (
+                // @ts-ignore
+                <div style={{ width: "100%", height: "100%", display: "flex", justifyContent: "center", alignItems: "center" }}>
+                    {/* 
+                    // @ts-ignore */}
+                    <circle-rail-spinner role="alert" aria-busy="true" aria-live="polite"></circle-rail-spinner>;
+                </div>
+            );
         } else if (!this.state.drag.over) {
             view = <p className="pt-instructions">Click and drag the block on the left to begin building a new page.</p>;
         } else {
@@ -543,11 +612,35 @@ class PageBuilder extends Component<{}, PageBuilderState> {
                 <header style={{ filter: this.state.submitting ? "blur(4px)" : "" }}>
                     <TitleInput value={this.state.title} callback={this.updateTitle.bind(this)} />
                     <div>
-                        <button onClick={this.handleReset} className="pt-bttn -danger -text mr-1">
-                            Reset Page
+                        <button onClick={this.handleURLPrompt} className="pt-bttn -text -grey -icon-only -round">
+                            <i>
+                                <svg aria-hidden="true" focusable="false" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
+                                    <path
+                                        fill="currentColor"
+                                        d="M503.691 189.836L327.687 37.851C312.281 24.546 288 35.347 288 56.015v80.053C127.371 137.907 0 170.1 0 322.326c0 61.441 39.581 122.309 83.333 154.132 13.653 9.931 33.111-2.533 28.077-18.631C66.066 312.814 132.917 274.316 288 272.085V360c0 20.7 24.3 31.453 39.687 18.164l176.004-152c11.071-9.562 11.086-26.753 0-36.328z"
+                                    ></path>
+                                </svg>
+                            </i>
                         </button>
-                        <button onClick={this.createPage} className="pt-bttn -solid -primary">
-                            Create Page
+                        <button onClick={this.handleReset} className="pt-bttn -danger -text -icon-only -round" style={{ marginLeft: "0.25rem" }}>
+                            <i>
+                                <svg aria-hidden="true" focusable="false" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512">
+                                    <path
+                                        fill="currentColor"
+                                        d="M432 32H312l-9.4-18.7A24 24 0 0 0 281.1 0H166.8a23.72 23.72 0 0 0-21.4 13.3L136 32H16A16 16 0 0 0 0 48v32a16 16 0 0 0 16 16h416a16 16 0 0 0 16-16V48a16 16 0 0 0-16-16zM53.2 467a48 48 0 0 0 47.9 45h245.8a48 48 0 0 0 47.9-45L416 128H32z"
+                                    ></path>
+                                </svg>
+                            </i>
+                        </button>
+                        <button onClick={this.createPage} className="pt-bttn -text -primary -icon-only -round" style={{ marginLeft: "0.25rem" }}>
+                            <i>
+                                <svg aria-hidden="true" focusable="false" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512">
+                                    <path
+                                        fill="currentColor"
+                                        d="M433.941 129.941l-83.882-83.882A48 48 0 0 0 316.118 32H48C21.49 32 0 53.49 0 80v352c0 26.51 21.49 48 48 48h352c26.51 0 48-21.49 48-48V163.882a48 48 0 0 0-14.059-33.941zM224 416c-35.346 0-64-28.654-64-64 0-35.346 28.654-64 64-64s64 28.654 64 64c0 35.346-28.654 64-64 64zm96-304.52V212c0 6.627-5.373 12-12 12H76c-6.627 0-12-5.373-12-12V108c0-6.627 5.373-12 12-12h228.52c3.183 0 6.235 1.264 8.485 3.515l3.48 3.48A11.996 11.996 0 0 1 320 111.48z"
+                                    ></path>
+                                </svg>
+                            </i>
                         </button>
                     </div>
                 </header>
